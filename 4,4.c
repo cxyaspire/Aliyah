0f5e6c0d674fda564f7d036f87a5d073c71182c2
cxyaspire <18250437672@139.com>
2026-09-19 17:58:40 +0800

    全新仓库，记录C语言练习

--- 4,4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	char ch;
-	scanf("%c",&ch);
-	ch=(ch>='A'&&ch<='Z')?(ch+32):ch;//固定格式：表达式1？表达式2：表达式3，1为真则2，反之为3 
-	printf("%c\n",ch);
-	return 0;
-}
--- hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	printf("欢迎来到C语言世界\n");
-	return 0;
-	
-}
