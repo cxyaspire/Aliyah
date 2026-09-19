b1ec6861cf737c5623a609b30265e270c38e8a75
cxyaspire <18250437672@139.com>
2026-09-19 20:43:44 +0800

    my name and lucky

--- 9.19c语言编程基础与开发环境练习.c
+++ 9.19c语言编程基础与开发环境练习.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 int main(void)
-{printf("Say my name!\n");//错误二：老是忘记反斜杠是哪个 
- printf("Yao.");
- return 0;//错误一：忘记return在括号里面还是外面（已修正） 
+{printf("Say my name!\n");//错误一：老是忘记反斜杠是哪个 
+ printf("Yao.\n");
+ 
+ //使用多个printf进行输出 
+ printf("*        *      *  ********  *    *   *          * \n");
+ printf("*        *      *  *         *   *     *       *   \n");
+ printf("*        *      *  *         *  *        *   *     \n");
+ printf("*        *      *  *         * *           *       \n");
+ printf("*        *      *  *         *  *          *       \n");
+ printf("*        *      *  *         *   *         *       \n");
+ printf("*******  ********  ********  *     *       *       \n");
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;//错误二：忘记return在括号里面还是外面（已修正） 
 }
 
